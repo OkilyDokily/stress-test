ea939b7dc24ab109d7223a7b76a1d3223c9cb6be
me <me@me.me.com>
2020-04-27 15:40:51 -0700

    get all inputs and then get values for each category

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
+function returnFeedback(mental,physical,coping){
+  
+  var mentalLength = mental.filter("[name=mental]:checked").length / mental.filter("[name=mental]").length;
+  var physicalLength = mental.filter("[name=physical]:checked").length / mental.filter("[name=physical]").length;
+  var copingLength = mental.filter("[name=coping]:checked").length / mental.filter("[name=coping]").length;
+
+  var mentalIsHigh = (mentalLength > .7);
+  var physicalIsHigh = (physicalLength > .7);
+  var copingIsHigh = (mentalLength > .7);
+
+  
+}
+
 $(document).ready(function(){
   $("form").submit(function(e){
-    
+   e.preventDefault();
+   var all = $("input");
+   returnFeedback(all);
   });
 })
