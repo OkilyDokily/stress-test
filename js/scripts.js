0dbbae11705c03942a2991a859275a76d29fa9c1
me <me@me.me.com>
2020-04-28 13:25:33 -0700

    factor out business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,43 @@
-function returnFeedback(mental,physical,coping){
+function returnFeedback(all){
   
-  var mentalLength = mental.filter("[name=mental]:checked").length / mental.filter("[name=mental]").length;
-  var physicalLength = mental.filter("[name=physical]:checked").length / mental.filter("[name=physical]").length;
-  var copingLength = mental.filter("[name=coping]:checked").length / mental.filter("[name=coping]").length;
+  var mentalLength = all.filter("[name=mental]:checked").length / all.filter("[name=mental]").length;
+  var physicalLength = all.filter("[name=physical]:checked").length / all.filter("[name=physical]").length;
+  var copingLength = all.filter("[name=coping]:checked").length / all.filter("[name=coping]").length;
 
-  var mentalIsHigh = (mentalLength > .7);
-  var physicalIsHigh = (physicalLength > .7);
-  var copingIsHigh = (mentalLength > .7);
+  var mentalIsHigh = (mentalLength > .4);
+  var physicalIsHigh = (physicalLength > .4);
+  var copingIsHigh = (mentalLength > .4);
 
-  
+  var results = [];
+  if(mentalIsHigh){
+      results.push("You have a lot of mental stress")
+  }
+  if(physicalIsHigh){
+    results.push("You have a lot of physical stress")
+  }
+  if((mentalIsHigh || physicalIsHigh) && copingIsHigh){
+    results.push("However it looks like you are doing some good coping exercizes to deal with it.")
+  }
+  if ((mentalIsHigh || physicalIsHigh) && (copingIsHigh === false)){
+    results.push("You really could stand to use more coping exercizes.")
+  }
+  if((mentalIsHigh && physicalIsHigh) === false && copingIsHigh){
+    results.push("You don't have any significant stress but it looks like you are doing exercizes that may be preventing that from happening. Keep up the good word")
+  }
+  return results;
 }
 
 $(document).ready(function(){
   $("form").submit(function(e){
    e.preventDefault();
    var all = $("input");
-   returnFeedback(all);
+   var results = returnFeedback(all);
+   results.forEach(function(result){
+     $("#results").append("<p>"+ result +"</p>");
+   })
+
+   function clear(){
+     $("#results p").remove();
+   }
   });
 })
